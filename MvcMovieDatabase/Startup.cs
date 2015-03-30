31b39548cebd48ed522b52e83ce2d9fd8843b675
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-30 10:11:52 -0400

    Start templates

---
+++ MvcMovieDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovieDatabase.Startup))]
+namespace MvcMovieDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
