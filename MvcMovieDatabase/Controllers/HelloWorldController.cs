8fbabf27d01776554d111171c7f3596f3bc8dfd7
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-30 11:37:13 -0400

    added helloworld controller
    displays text when sent to helloworld page

---
+++ MvcMovieDatabase/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovieDatabase.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <strong>default</strong> action...";
+        }
+    }
+}
