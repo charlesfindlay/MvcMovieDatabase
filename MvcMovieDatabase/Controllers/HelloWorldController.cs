943ca0d8e5a3092a4d2ebdfd4fb4b7a6449b8927
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-30 16:51:20 -0400

    Adding a view

--- MvcMovieDatabase/Controllers/HelloWorldController.cs
+++ MvcMovieDatabase/Controllers/HelloWorldController.cs
@@ -9,15 +9,19 @@ namespace MvcMovieDatabase.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <strong>default</strong> action...";
+            return View();
         }
 
         // GET: Helloworld/welcome
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello, " + name + ", ID: " + ID);
+
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;            
+            
+            return View();
         }
     }
 }
