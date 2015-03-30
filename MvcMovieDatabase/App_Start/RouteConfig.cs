b4b58fef958fabba90390c5fd11ea37a810a1930
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-30 14:30:54 -0400

    test project plus more controller changes

--- MvcMovieDatabase/App_Start/RouteConfig.cs
+++ MvcMovieDatabase/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace MvcMovieDatabase
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Welcome",
+                url: "{controller}/{action}/{name}/{id}");
         }
     }
 }
---
+++ MyTestProject/Class1.cs
@@ -0,0 +1,24 @@
+﻿using MvcMovieDatabase.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace MyTestProject
+{
+    public class Class1
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            var controller = new HomeController();
+            var someView = controller.Index();
+
+            var theView = (ViewResult)someView;
+            Assert.NotNull(someView);
+        }
+    }
+}
