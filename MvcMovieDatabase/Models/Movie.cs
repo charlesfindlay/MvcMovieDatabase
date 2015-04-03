19feff2b76b3a006c5825e9a7023fc02e940c2a8
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-03 10:40:09 -0400

    Added validation to movie model

--- MvcMovieDatabase/Models/Movie.cs
+++ MvcMovieDatabase/Models/Movie.cs
@@ -7,14 +7,25 @@ namespace MvcMovieDatabase.Models
     public class Movie  // This class movie maps to a table named movie in the database
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
